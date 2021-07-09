5de96be513567d1c253936b6a144022ebfbec747
sinistergaz <75819404+gary-socialmindr@users.noreply.github.com>
2021-07-09 18:14:27 +1000

    URL config change for password reset

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f7495ea5a70d3625399d4efbf2071de6'),
+      secret: env("ADMIN_JWT_SECRET", "f7495ea5a70d3625399d4efbf2071de6"),
     },
   },
 });
